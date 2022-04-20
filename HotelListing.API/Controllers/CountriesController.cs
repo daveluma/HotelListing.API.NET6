df9be51eb01988b8e878bea2056d4d5a2c74f0d6
daveluma <daveluma@gmail.com>
2022-04-20 19:31:24 -0400

    Finished Delete and Put request review

--- HotelListing.API/Controllers/CountriesController.cs
+++ HotelListing.API/Controllers/CountriesController.cs
@@ -48,7 +48,7 @@ namespace HotelListing.API.Controllers
         {
             if (id != country.Id)
             {
-                return BadRequest();
+                return BadRequest("Invalid Record Id");
             }
 
             _context.Entry(country).State = EntityState.Modified;
